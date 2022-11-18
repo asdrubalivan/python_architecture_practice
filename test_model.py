acb2ef70c7d3788df0be64ab7aab2a5e386787fa
Asdrúbal Iván Suárez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2022-11-18 14:10:12 -0400

    Adding first hypothesis tests

--- test_model.py
+++ test_model.py
@@ -1,6 +1,35 @@
 from model import Batch, OrderLine
 from datetime import date
-from typing import Tuple
+from typing import Tuple, Callable, TypeVar
+from hypothesis import given, assume
+from hypothesis.strategies import composite, text, integers, SearchStrategy
+
+T = TypeVar('T')
+Draw = TypeVar('Draw', bound=Callable[[SearchStrategy[T]], T])
+
+OrderTestData = Tuple[Batch, OrderLine]
+
+@composite
+def _make_batch(draw: Draw, condition: Callable[[int, int], bool] = lambda x, y: True, batch_equal: bool = False) -> Tuple[Batch, OrderLine]:
+    batch_id = draw(text(max_size=10))
+    order_line = draw(text(max_size=10))
+    eta = date.today() # TODO change this
+    sku = draw(text(max_size=10))
+
+    batch_qty = draw(integers(min_value=1, max_value=100_000))
+    line_qty = draw(integers(min_value=1, max_value=100_000))
+
+    if batch_equal:
+        line_qty = batch_qty
+    assume(condition(batch_qty, line_qty))
+
+    batch = Batch(batch_id, sku, batch_qty, eta)
+    line = OrderLine(order_line, sku, line_qty)
+    
+    return batch, line
+
+    
+
 
 def make_batch_and_line(sku: str, batch_qty: int, line_qty: int) -> Tuple[Batch, OrderLine]:
     return (
@@ -12,15 +41,33 @@ def test_can_allocate_if_available_greater_than_required() -> None:
     large_batch, small_line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
     assert large_batch.can_allocate(small_line)
 
+@given(_make_batch(lambda batch_qty, line_qty: batch_qty > line_qty))
+def test_can_allocate_if_available_greater_than_required_hyp(data: Tuple[Batch, OrderLine]) -> None:
+    batch, orderline = data
+    assert batch.can_allocate(orderline)
+    
+
 def test_cannot_allocate_if_available_smaller_than_required() -> None:
     small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
+    assert small_batch.can_allocate(large_line) is False
+
+@given(_make_batch(lambda batch_qty, line_qty: batch_qty < line_qty))
+def test_cannot_allocate_if_available_smaller_than_required_hyp(data: OrderTestData) -> None:
+    small_batch, large_line = data
     assert small_batch.can_allocate(large_line) is False
 
 def test_can_allocate_if_available_equal_to_required() -> None:
     batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 2)
     assert batch.can_allocate(line)
 
-def test_cannot_allocate_if_skus_do_not_match() -> None:
-    batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
-    different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
-    assert batch.can_allocate(different_sku_line) is False
+@given(_make_batch(batch_equal=True))
+def test_can_allocate_if_available_equal_to_required_hyp(data: OrderTestData) -> None:
+    batch, line = data
+    assert batch.can_allocate(line)
+
+@given(_make_batch(condition=lambda batch_qty, order_qty: batch_qty >= order_qty), text())
+def test_cannot_allocate_if_skus_do_not_match(data: OrderTestData, new_sku: str) -> None:
+    batch, old_line = data
+    assume(batch.sku != new_sku)
+    line = OrderLine(old_line.orderid, new_sku, old_line.qty)
+    assert batch.can_allocate(line) is False
